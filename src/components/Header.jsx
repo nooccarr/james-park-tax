d76c15bb71651d32d96e8d3863342373c34350a5
Soohyun Lim <d2axasap@gmail.com>
2023-10-17 16:59:13 -0400

    Move home link from layout component to header component. Add more logic to header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-      Header
-      <Link to='/'>Home</Link>
+      <div>
+        <Link to='/'>
+          <h1>James Park</h1>
+          <h2>Tax & Accounting</h2>
+        </Link>
+      </div>
+      <div>
+        <span>CALL US TODAY</span>
+        <a href='tel:+17183591096'>(718) 359-1096</a>      
+      </div>
     </div>
   );
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,9 +6,6 @@ const Layout = () => {
       <nav>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
             <Link to='/about'>About</Link>
           </li>
           <li>
