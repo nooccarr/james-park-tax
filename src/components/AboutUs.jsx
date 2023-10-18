65946d8b54ad990b552abf0b93caaf253230170f
Soohyun Lim <d2axasap@gmail.com>
2023-10-18 16:58:26 -0400

    Add mock data to AboutUs component.

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,20 @@
 const AboutUs = () => {
   return (
     <div>
-      <h2>About Us</h2>
+      <div>
+        <h2>About Us</h2>
+      </div>
+      <main>
+        <p>Capicola cillum sunt, drumstick excepteur voluptate ea brisket tail tenderloin magna ut pig ut et.</p>
+        <p></p>
+        <p>Ground round occaecat sed mollit. Voluptate bacon capicola beef ribs aliqua venison rump, short loin culpa sunt incididunt consequat ball tip in. Hamburger id in exercitation buffalo, qui rump ut anim salami tail proident occaecat.","Kevin turducken leberkas, sint boudin sed pancetta dolore ex buffalo ham hock exercitation beef ribs laborum drumstick.</p>
+        <p></p>
+        <p>Fugiat in cillum venison pastrami, cupim tenderloin in duis. Pariatur pork loin ribeye, voluptate veniam in hamburger tail meatloaf cupidatat meatball. Qui dolore veniam pancetta jowl shank sed, anim capicola landjaeger magna fugiat. Chuck strip steak swine ex, capicola dolore turkey sunt pig velit.", "In ut fugiat velit incididunt. Jerky ea proident nulla, exercitation ullamco lorem hamburger burgdoggen cupim fatback sausage nisi ham hock. Ad bacon turducken landjaeger elit capicola nulla ea shoulder ut. Ball tip sirloin chislic commodo tri-tip pancetta enim excepteur in chicken nisi dolore labore.", "Anim deserunt chislic nostrud cupidatat. Nulla biltong cupim ham hock shankle, chislic officia enim nisi cow in ut kielbasa.</p>
+        <p></p> 
+        <p>Kevin velit ut pariatur ut anim, pancetta dolore veniam shoulder laboris leberkas ham hock meatloaf picanha. Andouille meatball dolore porchetta chislic frankfurter nostrud jowl laborum flank meatloaf voluptate culpa.  Flank short loin drumstick, esse t-bone ex leberkas jerky aute picanha pork loin spare ribs tempor. Fatback short ribs bresaola tenderloin shoulder turducken nostrud fugiat, mollit et flank. Tempor fatback esse officia nisi bresaola shankle consectetur aliqua pancetta drumstick.", "Est tail meatball dolor ad prosciutto alcatra beef ribs chuck nostrud pork chop.</p>
+        <p></p>
+        <p>Ham hock chislic occaecat non pig. Aliqua shoulder brisket biltong burgdoggen, filet mignon minim jerky ut id sunt labore nostrud meatloaf ut. Pork jerky cillum in pig leberkas ball tip ut beef tenderloin ad consequat deserunt cupim boudin.</p>
+      </main>
     </div>
   );
 };
