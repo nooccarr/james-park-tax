ed9c440aaaa6b2af773bcf94a768be690c6c46e2
Soohyun Lim <d2axasap@gmail.com>
2023-10-19 16:15:37 -0400

    Configure Vite to use the proxy server.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: ''
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: './src/index.jsx'
+    }
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4000'
+    }
+  }
 });
--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
-const app = require('./express.js');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+app.get("/message", (_, res) => res.send("Hello from express!"));
+app.get("/", (_, res) => res.send("Hello world!"));
 
 const PORT = 4000;
 const server = app.listen(PORT, () => {
