4ee262eb51aebe486fe054e522b51e94787fb343
Soohyun Lim <d2axasap@gmail.com>
2023-10-19 16:57:18 -0400

    Make server to handle routes for client.

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,16 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: './src/index.jsx'
-    }
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:4000'
-    }
-  }
+  plugins: [react()]
 });
--- server/server.js
+++ server/server.js
@@ -6,10 +6,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-app.get("/message", (_, res) => res.send("Hello from express!"));
-app.get("/", (_, res) => res.send("Hello world!"));
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"), err => {
+      if (err) {
+          console.log(err);
+      }
+  });
+});
 
 const PORT = 4000;
 const server = app.listen(PORT, () => {
