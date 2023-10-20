1ae507622d93824de9b524959f9130ffc6425fbc
Soohyun Lim <d2axasap@gmail.com>
2023-10-20 09:17:49 -0400

    Add comments to server.js

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const path = require('path');
 
 const app = express();
 
+// Middleware that looks at requests where the Content-Type header matches the type option, and converts the body into a JavaScript object
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the React app
 app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
 
+// Handle client routing, return all requests to the app
 app.get("/*", (_, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"), err => {
       if (err) {
@@ -16,7 +19,8 @@ app.get("/*", (_, res) => {
   });
 });
 
-const PORT = 4000;
+const { PORT = 4000 } = process.env;
+
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
