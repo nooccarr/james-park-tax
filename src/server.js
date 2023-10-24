64398444d50f948395636a4d51775ff60140bf8d
Soohyun Lim <d2axasap@gmail.com>
2023-10-23 20:04:48 -0400

    Modify server configurations.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const { PORT = 4000 } = process.env;
+
 const app = express();
 
 // Middleware that looks at requests where the Content-Type header matches the type option, and converts the body into a JavaScript object
@@ -20,8 +22,6 @@ app.get('*', (_, res) => {
     if (err) console.log(err);
   });
 });
-
-const { PORT = 4000 } = process.env;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const { PORT = 4000 } = process.env;
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist'
+  }
 });
