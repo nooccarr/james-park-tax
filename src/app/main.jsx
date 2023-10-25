4414d125e41c8bab217c0a997c751b5bcf28a101
Soohyun Lim <d2axasap@gmail.com>
2023-10-25 16:25:39 -0400

    Use React Bootstrap for styling.

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './components/App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
