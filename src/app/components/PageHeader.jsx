7cd382c3d40a32e988edf890b226e401397f8600
Soohyun Lim <d2axasap@gmail.com>
2023-12-06 12:30:18 -0500

    Make category name in page header image to move to center when it's in mobile view.

--- src/app/components/PageHeader.jsx
+++ src/app/components/PageHeader.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const PageHeader = ({ title }) => {
-  
+  const [isMobile, setisMobile] = useState(false);
+
+  useEffect(() => {
+    const onWindowResize = () => {
+      window.innerWidth <= 767 ? setisMobile(true) : setisMobile(false);
+    };
+    window.addEventListener('resize', onWindowResize);
+    return () => window.removeEventListener('resize', onWindowResize);
+  }, []);
+
+  const mobile = isMobile ? 'text-center' : '';
+
   return (
     <Container as='section' className='py-5'>
-      <Row as='h2' className='ps-2 py-5' style={{ color: '#FFF9D1', fontSize: '3em' }}>{title}</Row>
+      <Row className={`ps-2 py-5 ${mobile}`} style={{ color: '#FFF9D1', fontSize: '3em' }}>
+        <Col className='p-0'>{title}</Col>
+      </Row>
     </Container>
   );
 };
