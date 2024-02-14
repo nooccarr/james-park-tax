2e7b0f904450925c614932a595a3ed50a0d9c641
Soohyun Lim <d2axasap@gmail.com>
2024-02-14 12:58:46 -0500

    Restyle footer section with Tailwind

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,16 +5,16 @@ import { useState, useEffect } from 'react';
 import '../styles/footer.css';
 
 const Footer = () => {
-  const [isMobile, setisMobile] = useState(false);
+  // const [isMobile, setisMobile] = useState(false);
 
-  useEffect(() => {
-    window.innerWidth <= 767 ? setisMobile(true) : setisMobile(false);
-    const onWindowResize = () => {
-      window.innerWidth <= 767 ? setisMobile(true) : setisMobile(false);
-    };
-    window.addEventListener('resize', onWindowResize);
-    return () => window.removeEventListener('resize', onWindowResize);
-  }, []);
+  // useEffect(() => {
+  //   window.innerWidth <= 767 ? setisMobile(true) : setisMobile(false);
+  //   const onWindowResize = () => {
+  //     window.innerWidth <= 767 ? setisMobile(true) : setisMobile(false);
+  //   };
+  //   window.addEventListener('resize', onWindowResize);
+  //   return () => window.removeEventListener('resize', onWindowResize);
+  // }, []);
 
   const today = new Date();
 
@@ -37,48 +37,26 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        {isMobile ? (
-          <>
-            <div className="pt-5 pb-2">
-              <div className="text-center">
-                <div className="footer-location">LOCATIONS</div>
-              </div>
-            </div>
-            <div>
-              <div className="text-center">{newYorkOffice()}</div>
-            </div>
-            <div>
-              <div className="text-center">{newJerseyOffice()}</div>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="pt-5 pb-2">
-              <div md={{ offset: 3, span: 3 }}>
-                <div className="footer-location">LOCATIONS</div>
-              </div>
-            </div>
-            <div className="mb-5">
-              <div md={{ offset: 3, span: 3 }}>{newYorkOffice()}</div>
-              <div md={{ span: 3 }}>{newJerseyOffice()}</div>
-            </div>
-          </>
-        )}
-
-        {isMobile ? (
-          <>
-            <div className="pb-2 text-center copyright-text">
-              For general inquiries call
-            </div>
-            <div className="pb-2 text-center copyright-text">
-              718-359-1096 / 201-625-3060
-            </div>
-          </>
-        ) : (
-          <div className="pb-2 text-center copyright-text">
-            For general inquiries call 718-359-1096 / 201-625-3060
+        <>
+          <div className="text-center pt-5 pb-2">
+            <div className="footer-location">LOCATIONS</div>
           </div>
-        )}
+
+          <div className="flex flex-col md:flex-row justify-center md:gap-20">
+            <div className="text-center">{newYorkOffice()}</div>
+
+            <div className="text-center">{newJerseyOffice()}</div>
+          </div>
+        </>
+
+        <div className="flex flex-wrap justify-center gap-1">
+          <div className="pb-2 text-center copyright-text">
+            For general inquiries call
+          </div>
+          <div className="pb-2 text-center copyright-text">
+            718-359-1096 / 201-625-3060
+          </div>
+        </div>
 
         <div className="pb-5 text-center copyright-text">
           &copy; {today.getFullYear()} James Park Tax & Accounting, Inc.
