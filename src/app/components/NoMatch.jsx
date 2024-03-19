e44b3d15b6d3491aeb72aa8626ab2ef438a3803b
Soohyun Lim <d2axasap@gmail.com>
2024-03-19 16:58:12 -0400

    Modify config files. Might need to deploy Express backend elsewhere

--- src/app/components/NoMatch.jsx
+++ src/app/components/NoMatch.jsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom';
 
 const NoMatch = () => {
   return (
-    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div class="text-center">
-        <p class="font-bold header-span text-5xl">404</p>
-        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <p className="font-bold header-span text-5xl">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
           Page not found
         </h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">
+        <p className="mt-6 text-base leading-7 text-gray-600">
           Sorry, we couldn’t find the page you’re looking for.
         </p>
-        <div class="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link to="/">
             <div className="article-button">Go back home</div>
           </Link>
