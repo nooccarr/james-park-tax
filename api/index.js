b9c33854c7d84d560d608d736fb7ef8d1d5bfdc2
Soohyun Lim <d2axasap@gmail.com>
2024-03-21 19:38:49 -0400

    Delete compression

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const dbConn = require('./config/dbConn');
@@ -7,7 +6,6 @@ const { PORT = 4000 } = process.env;
 
 const app = express();
 
-app.use(compression());
 // Middleware that looks at requests where the Content-Type header matches the type option, and converts the body into a JavaScript object
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
