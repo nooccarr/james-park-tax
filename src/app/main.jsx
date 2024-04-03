93bf8304501504a3af126278452bb441a9e2dbbb
Soohyun Lim <d2axasap@gmail.com>
2024-04-03 15:43:58 -0400

    Use Clerk as authentication and user management

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,18 +2,27 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import { NextUIProvider } from '@nextui-org/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
 
 // Importing the Tailwind CSS
 import './output.css';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <NextUIProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   </NextUIProvider>
 );
