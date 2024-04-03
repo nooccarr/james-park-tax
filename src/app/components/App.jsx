8c264beee33f43aaf684ba9cbffc106c580c054c
Soohyun Lim <d2axasap@gmail.com>
2024-04-03 16:03:49 -0400

    Create a component for kakao canvas

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -13,10 +13,10 @@ import PostList from './PostList';
 import Post from './Post';
 import NewPost from './NewPost';
 import EditPost from './EditPost';
-import Login from './Login';
+// import Login from './Login';
+import KakaoCanvas from './KakaoCanvas';
 import NoMatch from './NoMatch';
 import Footer from './Footer';
-import Kakaotalk from '../images/kakaotalk-offcanvas.png';
 import useFetchData from '../hooks/useFetchData';
 import '../styles/app.css';
 import Posts from '../data/posts';
@@ -128,29 +128,13 @@ const App = () => {
           <Route path="contact-us" element={<ContactUs />} />
           <Route path="new-post" element={<NewPost />} />
           <Route path="edit-post" element={<EditPost />} />
-          <Route path="login" element={<Login />} />
+          {/* <Route path="login" element={<Login />} /> */}
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
       <Footer />
       {showKakaoCanvas && (
-        <div className="offcanvas-background-image md:absolute w-[370px] h-screen top-0 right-0 z-10 p-5">
-          <div className="flex justify-end">
-            <div onClick={handleKakaoCanvasClose}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 384 512"
-                className="h-5 w-5 hover:cursor-pointer"
-                fill="#fff9d1"
-              >
-                <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
-              </svg>
-            </div>
-          </div>
-          <div className="mt-12 flex justify-center">
-            <img src={Kakaotalk} alt="" className="offcanvas-kakao-image" />
-          </div>
-        </div>
+        <KakaoCanvas handleKakaoCanvasClose={handleKakaoCanvasClose} />
       )}
     </div>
   );
