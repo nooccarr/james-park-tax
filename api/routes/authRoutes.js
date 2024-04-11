5d77c6f22660cfdda2a279901b33c2f5eb4d25a0
Soohyun Lim <d2axasap@gmail.com>
2024-04-11 13:52:37 -0400

    Handle the Login route in the backend API

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.route('/signup').post(authController.signUp);
+router.route('/login').post(authController.Login);
 
 module.exports = router;
