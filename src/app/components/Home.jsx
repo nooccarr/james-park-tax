98b602fb23dcf655ae7f543a6c87a875f1d3b9f8
Soohyun Lim <d2axasap@gmail.com>
2025-03-05 17:32:55 -0500

    fix(carousel): fix mobile view displaying two images stacked instead of carousel

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -37,78 +37,134 @@ const Home = () => {
 
   return (
     <main className="animate-fadeIn select-none">
-      <Carousel
-        autoPlay
-        interval="10000"
-        transitionTime="1000"
-        infiniteLoop
-        // emulateTouch
-        showThumbs={false}
-        // showArrows={false}
-        showStatus={false}
-      >
-        <div>
-          <div className="min-h-[500px] aspect-video">
-            <img
-              className="brightness-90 w-full h-full object-cover"
-              src={LandingImgOne}
-              fetchpriority="high"
-              srcSet={`${LandingImgOneSmall} 500w, ${LandingImgOneMedium} 1000w, ${LandingImgOne} 1500w`}
-              sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-              loading="eager"
-              alt="Skyline of lower Manhattan, New York City at dusk, featuring the One World Trade Center and illuminated buildings reflecting in the water."
-            />
+      <div className="hidden md:block">
+        <Carousel
+          autoPlay
+          interval="10000"
+          transitionTime="1000"
+          infiniteLoop
+          // emulateTouch
+          showThumbs={false}
+          // showArrows={false}
+          showStatus={false}
+        >
+          <div>
+            <div className="min-h-[500px] aspect-video">
+              <img
+                className="brightness-90 w-full h-full object-cover"
+                src={LandingImgOne}
+                fetchpriority="high"
+                srcSet={`${LandingImgOneSmall} 500w, ${LandingImgOneMedium} 1000w, ${LandingImgOne} 1500w`}
+                sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
+                loading="eager"
+                alt="Skyline of lower Manhattan, New York City at dusk, featuring the One World Trade Center and illuminated buildings reflecting in the water."
+              />
+            </div>
+
+            {!showFormSuccess ? (
+              <button
+                onClick={handleButtonClick}
+                className="absolute top-[50%] md:top-[70%] left-[50%] text-md md:text-xl font-semibold home-contact-form-button p-3 md:p-5 min-w-[222px]"
+                style={{ transform: 'translate(-50%, -50%)' }}
+              >
+                <h2 className="">
+                  <FontAwesomeIcon className="pe-2" icon={faCalendarDays} />
+                  Schedule a Consultation
+                </h2>
+              </button>
+            ) : (
+              <h2
+                className="form-submit-message absolute top-[50%] md:top-[70%] left-[50%] min-w-[222px]"
+                style={{ transform: 'translate(-50%, -50%)' }}
+              >
+                Thank you for your message.
+                <br />
+                It has been sent.
+              </h2>
+            )}
           </div>
 
-          {!showFormSuccess ? (
-            <button
-              onClick={handleButtonClick}
-              className="absolute top-[50%] md:top-[70%] left-[50%] text-md md:text-xl font-semibold home-contact-form-button p-3 md:p-5 min-w-[222px]"
+          <div>
+            <div className="min-h-[500px] aspect-video">
+              <img
+                className="brightness-90 w-full h-full object-cover"
+                src={LandingImgTwo}
+                fetchpriority="high"
+                srcSet={`${LandingImgTwoSmall} 500w, ${LandingImgTwoMedium} 1000w, ${LandingImgTwo} 1500w`}
+                sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
+                loading="eager"
+                alt="Close-up of two people shaking hands across a wooden table, symbolizing agreement or partnership."
+              />
+            </div>
+            <div
+              className="absolute top-[50%] md:top-[70%] left-[50%] w-full text-lg md:text-3xl font-semibold flex flex-col md:gap-4"
               style={{ transform: 'translate(-50%, -50%)' }}
             >
-              <h2 className="">
-                <FontAwesomeIcon className="pe-2" icon={faCalendarDays} />
-                Schedule a Consultation
-              </h2>
-            </button>
-          ) : (
-            <h2
-              className="form-submit-message absolute top-[50%] md:top-[70%] left-[50%] min-w-[222px]"
-              style={{ transform: 'translate(-50%, -50%)' }}
-            >
-              Thank you for your message.
-              <br />
-              It has been sent.
-            </h2>
-          )}
+              <h1 className="home-carousel-h1">
+                한국거주 영주권, 시민권자 세금보고
+              </h1>
+
+              <h1 className="home-carousel-h1">증여, 상속세, 양도세 상담</h1>
+              <h1 className="home-carousel-h1">메디케어 건강보험 안내</h1>
+            </div>
+          </div>
+        </Carousel>
+      </div>
+
+      <div className="block md:hidden">
+        <div className="aspect-square max-h-[767px]">
+          <img
+            className="brightness-90 w-full h-full object-cover"
+            src={LandingImgOne}
+            fetchpriority="high"
+            srcSet={`${LandingImgOneSmall} 500w, ${LandingImgOneMedium} 1000w, ${LandingImgOne} 1500w`}
+            sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
+            loading="eager"
+            alt="Skyline of lower Manhattan, New York City at dusk, featuring the One World Trade Center and illuminated buildings reflecting in the water."
+          />
         </div>
 
-        <div>
-          <div className="min-h-[500px] aspect-video">
-            <img
-              className="brightness-90 w-full h-full object-cover"
-              src={LandingImgTwo}
-              fetchpriority="high"
-              srcSet={`${LandingImgTwoSmall} 500w, ${LandingImgTwoMedium} 1000w, ${LandingImgTwo} 1500w`}
-              sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
-              loading="eager"
-              alt="Close-up of two people shaking hands across a wooden table, symbolizing agreement or partnership."
-            />
-          </div>
-          <div
-            className="absolute top-[50%] md:top-[70%] left-[50%] w-full text-lg md:text-3xl font-semibold flex flex-col md:gap-4"
+        {!showFormSuccess ? (
+          <button
+            onClick={handleButtonClick}
+            className="absolute top-[50%] left-[50%] font-semibold home-contact-form-button p-3 md:p-5 min-w-[222px]"
             style={{ transform: 'translate(-50%, -50%)' }}
           >
+            <h2 className="">
+              <FontAwesomeIcon className="pe-2" icon={faCalendarDays} />
+              Schedule a Consultation
+            </h2>
+          </button>
+        ) : (
+          <h2
+            className="form-submit-message absolute top-[50%] left-[50%] min-w-[222px]"
+            style={{ transform: 'translate(-50%, -50%)' }}
+          >
+            Thank you for your message.
+            <br />
+            It has been sent.
+          </h2>
+        )}
+
+        <div className="aspect-square relative max-h-[767px]">
+          <img
+            className="brightness-90 w-full h-full object-cover"
+            src={LandingImgTwo}
+            fetchpriority="high"
+            srcSet={`${LandingImgTwoSmall} 500w, ${LandingImgTwoMedium} 1000w, ${LandingImgTwo} 1500w`}
+            sizes="(max-width: 500px) 500px, (max-width: 1000px) 1000px, 1500px"
+            loading="eager"
+            alt="Close-up of two people shaking hands across a wooden table, symbolizing agreement or partnership."
+          />
+          <div className="absolute top-[50%] w-full justify-center items-center text-lg text-center font-semibold flex flex-col">
             <h1 className="home-carousel-h1">
               한국거주 영주권, 시민권자 세금보고
             </h1>
-
             <h1 className="home-carousel-h1">증여, 상속세, 양도세 상담</h1>
             <h1 className="home-carousel-h1">메디케어 건강보험 안내</h1>
           </div>
         </div>
-      </Carousel>
-
+      </div>
       <ContactForm
         showContactForm={showContactForm}
         handleContactFormClose={handleContactFormClose}
